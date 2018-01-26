1bbdf8478f7dc13505586da2386551c4b4f9b0fb
LG-UINT <Guang.LI@uint.fr>
2018-01-26 17:32:46 +0100

    Sort out the code

---
+++ Inc/freertos_task.h
@@ -0,0 +1,18 @@
+
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __FREERTOS_TASK_H
+#define __FREERTOS_TASK_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif 
+
+void createTasks( void );
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __FREERTOS_TASK_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
